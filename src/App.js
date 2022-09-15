608f307e2e0ecf429c9d604575c8b6b9d457159f
solovyovsdom <joelborofsky@mac.com>
2022-09-15 12:55:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/home" element={<Home />} exact />
+          <Route path="/about" component={<About />} exact />
+          <Route path="/skills" component={<Skills />} exact />
+          <Route path="/contact" component={<Contact />} exact />
+          <Route path="/signup" component={<Signup />} exact />
+        </Routes>
+      </Router>
     </div>
   );
 }
